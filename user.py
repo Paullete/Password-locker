99c3e8f6a9a855f594750bd3566ee6065582a08a
paullete <paulettenereah3@gmail.com>
2022-04-22 13:24:33 +0300

    details

--- user.py
+++ user.py
@@ -1,13 +1,14 @@
 from collections import UserList
-from typing_extensions import Self
+import selectors
+# from typing_extensions import Self
 
 
 class User:
     UserList=[]
     
     def_init_(self.user_name.password):
-        Self.user_name=user_name
-        Self.password=password
+        self.user_name=user_name
+        self.password=password
         
         def save_user(self):
             
