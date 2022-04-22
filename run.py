70e83e3e93e09e2bb81475ac02dabed492173c66
paullete <paulettenereah3@gmail.com>
2022-04-22 14:02:25 +0300

    details

--- run.py
+++ run.py
@@ -1,2 +1,16 @@
+from json.tool import main
 import random
 from user import User
+
+ief main():
+    
+    while True:
+        print("welcome to password locker!!!")
+        print('/n')
+        print("Select a short code to navigte through:to create new user use 'nu':To login to your account 'lg' or 'ex' to exit")
+        short_code=input().lower()
+        print('/n')
+        
+        if short_code == 'nu':
+            print('create username')
+            created_user_name = input()
