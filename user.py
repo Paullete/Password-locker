45de80824dded5c8a391cadc1b567440a506a2f6
paullete <paulettenereah3@gmail.com>
2022-04-22 13:13:42 +0300

    details

--- user.py
+++ user.py
@@ -1,6 +1,15 @@
 from collections import UserList
+from typing_extensions import Self
 
 
 class User:
     UserList=[]
+    
     def_init_(self.user_name.password):
+        Self.user_name=user_name
+        Self.password=password
+        
+        def save_user(self):
+            
+            User.UserList.append(self)
+        
