1b8cc0d867f6961ed959e767b56cafb1c45e75ac
paullete <paulettenereah3@gmail.com>
2022-04-22 13:39:10 +0300

    details

--- user.py
+++ user.py
@@ -4,15 +4,19 @@ from typing_extensions import Self
 
 
 class User:
-    '''class that generate new instance of user'''
+    '''
+    class that generate new instance of user
+    '''
     UserList=[]
     
     def_init_(self,user_name,password):
-        Self.user_name=user_name
-        Self.password=password
+    Self.user_name=user_name
+    Self.password=password
         
-        def save_user(self):
-            '''save_user method saves a ne user object to the user list'''
+    def save_user(self):
+        '''
+        save_user method saves a ne user object to the user list
+        '''
+           
             
-            User.UserList.append(self)
-        
+        User.User_List.append(self)
